43bd6555491881d74c2c59dfb3750333f938b28c
jobanjit-singh <singhjobanjit02@gmail.com>
2025-03-10 19:29:32 +0530

    responsive navbar and gsap

--- ReactProjects/airwings/src/components/CustomNavbar/CustomNavbar.js
+++ ReactProjects/airwings/src/components/CustomNavbar/CustomNavbar.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./styles.css";
 import ASSETS from "../../assets";
 import CustomButton from "../CustomButton/CustomButton";
 import { IoAirplaneSharp } from "react-icons/io5";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { MdCancel } from "react-icons/md";
+import gsap from "gsap";
 
 export default function CustomNavbar() {
+  const sideBarRef = useRef();
+
   const linkList = [
     {
       title: "Home",
@@ -26,11 +31,32 @@ export default function CustomNavbar() {
     },
   ];
 
+  const handleShowSideBar = () => {
+    gsap.to(sideBarRef.current, {
+      translateX: "0%",
+      opacity: 1,
+      duration: 0.5,
+    });
+  };
+
+  const handleHideSideBar = () => {
+    gsap.to(sideBarRef.current, {
+      translateX: "-100%",
+      opacity: 0,
+      duration: 0.5,
+    });
+  };
+
   return (
     <div className="navbarBaseContainer">
       <div className="navbarLogoContainer">
         <div className="navbarLogoBaseContainer">
           {/* <img src={ASSETS.mainLogo} alt="main-logo" /> */}
+          <HiOutlineMenuAlt2
+            className="navbarMenuIcon"
+            onClick={handleShowSideBar}
+            size={24}
+          />
           <h2>
             Air
             <span
@@ -64,6 +90,25 @@ export default function CustomNavbar() {
             backgroundColor={"transparent"}
             border={"2px solid var(--secondaryColor)"}
           />
+        </div>
+      </div>
+      <div className="navBarSideBarBaseContainer" ref={sideBarRef}>
+        <div className="navBarSideBarContainer">
+          <div className="navBarSideBarIconContainer">
+            <h1>
+              Air
+              <span
+                style={{
+                  color: "var(--secondaryColor)",
+                  textDecorationLine: "underline",
+                }}
+              >
+                Wings
+              </span>
+            </h1>
+            <MdCancel size={30} onClick={handleHideSideBar} />
+          </div>
+          <div className="navBarSideBarLinkContainer"></div>
         </div>
       </div>
     </div>
