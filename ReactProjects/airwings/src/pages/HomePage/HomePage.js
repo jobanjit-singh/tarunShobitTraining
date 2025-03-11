1de6d7279dbd1368538dae0ce1ff0ac86f2b4813
jobanjit-singh <singhjobanjit02@gmail.com>
2025-03-11 18:58:03 +0530

    useeffect done

--- ReactProjects/airwings/src/pages/HomePage/HomePage.js
+++ ReactProjects/airwings/src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // function HomePage() {
 //   // let count = 0;
@@ -23,12 +23,30 @@ import { useState } from "react";
 import "./styles.css";
 import CustomNavbar from "../../components/CustomNavbar/CustomNavbar";
 function HomePage() {
+  const [counter, setCounter] = useState(1);
+  const [counter2, setCounter2] = useState(1);
+  const [showResult, setShowResult] = useState(false);
+
+  useEffect(() => {
+    if (counter2 % 2 === 0) {
+      setShowResult(true);
+    } else {
+      setShowResult(false);
+    }
+  }, [counter2]);
+
   return (
     <div className="homePageBaseContainer">
       <div className="homePageNavbarContainer">
         <CustomNavbar />
       </div>
-      <div className="homePageRouteContainer"></div>
+      <div className="homePageRouteContainer">
+        <h1 style={{ color: "white" }}>{counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>Click Here</button>
+        <h1 style={{ color: "white" }}>Counter 2 Value = {counter2}</h1>
+        <button onClick={() => setCounter2(counter2 + 1)}>Click Here</button>
+        <h1 style={{ color: "white" }}>{showResult ? "EVEN" : "ODD"}</h1>
+      </div>
     </div>
   );
 }
